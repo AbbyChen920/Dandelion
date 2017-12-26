572b8375e0cf3a2f9dfc3bad3fbca510e3404771
陈芬 <Abby@chenfendeMacBook-Pro.local>
2017-12-26 23:30:36 +0800

    设置占位文字颜色

--- Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
+++ Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
@@ -17,7 +17,31 @@
     // 设置光标颜色
     self.tintColor = [UIColor whiteColor];
     
-    self.textColor = [UIColor redColor]; 
+    // 设置占位文字的颜色
+    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+    attributes[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attributes];
+    
+    
+//    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+//    attributes[NSForegroundColorAttributeName] = [UIColor yellowColor];
+//    attributes[NSBackgroundColorAttributeName] = [UIColor redColor];
+//    attributes[NSUnderlineStyleAttributeName] = @YES;
+//    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attributes];
+    
+//    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:self.placeholder];
+//    // 设置属性
+//    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+//    attributes[NSForegroundColorAttributeName] = [UIColor yellowColor];
+//    [string setAttributes:attributes range:NSMakeRange(0, 1)];
+//    
+//    NSMutableDictionary *attributes2 = [NSMutableDictionary dictionary];
+//    attributes2[NSBackgroundColorAttributeName] = [UIColor redColor];
+//    attributes2[NSUnderlineStyleAttributeName] = @1;
+//    [string setAttributes:attributes2 range:NSMakeRange(1, 1)];
+//    
+//    self.attributedPlaceholder = string;
+    
 }
 
 @end
