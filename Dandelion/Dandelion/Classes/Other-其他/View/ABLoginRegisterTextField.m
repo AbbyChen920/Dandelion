a920077443149064a72cfcef25f1d1872a0e6e44
陈芬 <Abby@chenfendeMacBook-Pro.local>
2018-01-03 23:34:06 +0800

    监听 uitextfield 的 内部事件04 -响应者

--- Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
+++ Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
@@ -12,7 +12,7 @@
 static NSString * const ABPlaceholderColorKey = @"placeholderLabel.textColor";
 
 @interface ABLoginRegisterTextField()
-@property (nonatomic,strong) id observer;
+
 @end
 
 @implementation ABLoginRegisterTextField
@@ -27,42 +27,24 @@ static NSString * const ABPlaceholderColorKey = @"placeholderLabel.textColor";
     // 设置默认的占位文字颜色
     [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
     
-    // object一定要设置为self,不然所有的文本框都会接受通知执行方法. 要规定只接受自己的文本框发出的通知
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(beginEditing) name:UITextFieldTextDidBeginEditingNotification object:self];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endEditing) name:UITextFieldTextDidEndEditingNotification object:self];
-   
-//    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(beginEditing) name:UITextFieldTextDidBeginEditingNotification object:self];
-//        
-//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endEditing) name:UITextFieldTextDidEndEditingNotification object:self];
-//    });
-    
-
-    // object对象发了名字为 name 的通知,就在queue队列中执行 block
-    // 这个方法的返回值是监听器
-    id observer = [[NSNotificationCenter defaultCenter] addObserverForName:UITextFieldTextDidBeginEditingNotification object:self queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
-        
-        // 移除通知
-        [[NSNotificationCenter defaultCenter] removeObserver:observer];
-    }];
-    
-}
--(void)dealloc
-{
-//    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    [[NSNotificationCenter defaultCenter] removeObserver:self.observer];
+    // 成为第一响应者: 开始编辑\弹出键盘\获得焦点
+    [self becomeFirstResponder];
+    // 不做第一响应者: 结束编辑\退出键盘\失去焦点
+    [self resignFirstResponder];
 }
 
-- (void)beginEditing
+
+//调用时刻: 成为第一响应者: 开始编辑\弹出键盘\获得焦点
+-(BOOL)becomeFirstResponder
 {
-    //    ABLog(@"%@ beginEditing",[note.object placeholder]);
     [self setValue:[UIColor whiteColor] forKeyPath:ABPlaceholderColorKey];
+    return [super becomeFirstResponder];
 }
 
-- (void)endEditing
+//调用时刻: 不做第一响应者: 结束编辑\退出键盘\失去焦点
+-(BOOL)resignFirstResponder
 {
-//    ABLog(@"%@ beginEditing",[note.object placeholder]);
     [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
+    return [super resignFirstResponder];
 }
 @end
