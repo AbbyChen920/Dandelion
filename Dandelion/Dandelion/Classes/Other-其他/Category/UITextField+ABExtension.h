b858e5487bb262cb5a32bc5e0af22c9e2828cdbc
陈芬 <Abby@chenfendeMacBook-Pro.local>
2018-01-06 00:43:37 +0800

    封装占位文字颜色属性

---
+++ Dandelion/Dandelion/Classes/Other-其他/Category/UITextField+ABExtension.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+ABExtension.h
+//  Dandelion
+//
+//  Created by 陈芬 on 18/1/5.
+//  Copyright © 2018年 陈芬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (ABExtension)
+
+// 占位文字颜色
+@property (nonatomic,strong) UIColor *placeholderColor;
+@end
--- Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
+++ Dandelion/Dandelion/Classes/Other-其他/View/ABLoginRegisterTextField.m
@@ -25,26 +25,25 @@ static NSString * const ABPlaceholderColorKey = @"placeholderLabel.textColor";
     self.tintColor = [UIColor whiteColor];
     
     // 设置默认的占位文字颜色
-    [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
-    
-    // 成为第一响应者: 开始编辑\弹出键盘\获得焦点
-    [self becomeFirstResponder];
-    // 不做第一响应者: 结束编辑\退出键盘\失去焦点
-    [self resignFirstResponder];
+//    [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
+    self.placeholderColor = [UIColor grayColor];
+
 }
 
 
 //调用时刻: 成为第一响应者: 开始编辑\弹出键盘\获得焦点
 -(BOOL)becomeFirstResponder
 {
-    [self setValue:[UIColor whiteColor] forKeyPath:ABPlaceholderColorKey];
+//    [self setValue:[UIColor whiteColor] forKeyPath:ABPlaceholderColorKey];
+    self.placeholderColor = [UIColor whiteColor];
     return [super becomeFirstResponder];
 }
 
 //调用时刻: 不做第一响应者: 结束编辑\退出键盘\失去焦点
 -(BOOL)resignFirstResponder
 {
-    [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
+//    [self setValue:[UIColor grayColor] forKeyPath:ABPlaceholderColorKey];
+    self.placeholderColor = [UIColor grayColor];
     return [super resignFirstResponder];
 }
 @end
