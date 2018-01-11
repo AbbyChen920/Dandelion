ae64bd256f1e8a389325064a978c174883e45185
陈芬 <Abby@chenfendeMacBook-Pro.local>
2018-01-11 18:13:46 +0800

    获得方块数据 - 把返回数据写成 plist

--- Dandelion/Dandelion/Classes/Me-我/View/ABMeFooterView.m
+++ Dandelion/Dandelion/Classes/Me-我/View/ABMeFooterView.m
@@ -7,12 +7,28 @@
 //
 
 #import "ABMeFooterView.h"
+#import <AFNetworking.h>
 
 @implementation ABMeFooterView
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
+        
+        // 参数
+        NSMutableDictionary *params = [NSMutableDictionary dictionary];
+        params[@"a"] = @"square";
+        params[@"c"] = @"topic";
+        
+        // 请求
+        [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nullable responseObject) {
+//            ABLog(@"请求成功- %@ %@", [responseObject class],responseObject);
+            
+            [responseObject writeToFile:@"/Users/Abby/Desktop/me.plist" atomically:YES];
+            
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            ABLog(@"请求失败 - %@",error);
+        }];
         
         
     }
