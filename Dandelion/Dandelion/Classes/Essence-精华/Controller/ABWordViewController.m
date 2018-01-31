9320bdf6cbb857e4de4b8e2852ebc1bc3391f603
陈芬 <Abby@chenfendeMacBook-Pro.local>
2018-01-31 23:46:28 +0800

    添加子控制器的 view

---
+++ Dandelion/Dandelion/Classes/Essence-精华/Controller/ABWordViewController.m
@@ -0,0 +1,52 @@
+//
+//  ABWordViewController.m
+//  Dandelion
+//
+//  Created by 陈芬 on 18/1/30.
+//  Copyright © 2018年 陈芬. All rights reserved.
+//
+
+#import "ABWordViewController.h"
+
+@interface ABWordViewController ()
+
+@end
+
+@implementation ABWordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    ABLogFunc
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 50;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    // 1.确定重用标识
+    static NSString *ID = @"cell";
+    
+    // 2.从缓存池中取
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    // 3.如果是空就手动创建
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell.backgroundColor = ABRandomColor;
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ - %zd",[self class], indexPath.row];
+    
+    return cell;
+    
+}
+
+@end
