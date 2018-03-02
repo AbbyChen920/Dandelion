d94ef91487a55399eed19883e039a3b57a69973d
陈芬 <Abby@chenfendeMacBook-Pro.local>
2018-03-02 21:10:39 +0800

    上拉刷新数据

--- Dandelion/Dandelion/Classes/Essence-精华/Controller/ABAllViewController.m
+++ Dandelion/Dandelion/Classes/Essence-精华/Controller/ABAllViewController.m
@@ -15,7 +15,10 @@
 
 @interface ABAllViewController ()
 // 所有的帖子数据
-@property (nonatomic,strong) NSArray <ABTopic *> *topics;
+@property (nonatomic,strong) NSMutableArray <ABTopic *> *topics;
+
+// maxtime: 用来加载下一页数据
+@property (nonatomic,copy) NSString *maxtime;
 @end
 
 @implementation ABAllViewController
@@ -40,6 +43,8 @@
     
     [self.tableView.mj_header beginRefreshing];
     
+    self.tableView.mj_footer = [MJRefreshAutoNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreTopics)];
+    
 }
 
 
@@ -54,6 +59,10 @@
     // 发送请求
     [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
 //        [responseObject writeToFile:@"/Users/Abby/Desktop/new_topics.plist" atomically:YES];
+        
+        // 存储 maxtime( 方便用来加载下一页数据)
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
         // 字典数组转模型数组
         self.topics = [ABTopic mj_objectArrayWithKeyValuesArray:responseObject[@"list"]];
         
@@ -71,6 +80,42 @@
     }];
 }
 
+
+- (void)loadMoreTopics
+{
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"maxtime"] = self.maxtime;
+    
+    // 发送请求
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+//        ABWriteToPlist(responseObject,@"more_topics");
+        
+        // 存储折页对应的 maxtime
+        self.maxtime = responseObject[@"info"][@"maxtime"];
+        
+        // 字典数->模型数组
+        NSArray<ABTopic *> *moreTopics = [ABTopic mj_objectArrayWithKeyValuesArray:responseObject[@"list"]];
+        [self.topics addObjectsFromArray:moreTopics];
+
+        
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // 让[刷新控件]结束刷新
+        [self.tableView.mj_footer endRefreshing];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        ABLog(@"请求失败 - %@",error);
+        
+        // 让[刷新控件]结束刷新
+        [self.tableView.mj_footer endRefreshing];
+        
+    }];
+
+}
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
